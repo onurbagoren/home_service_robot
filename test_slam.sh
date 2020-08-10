c6f5b225ed116130cf48f6679290801bc6a08f6d
Onur Bagoren <onurbagoren3@gmail.com>
2020-08-10 19:23:40 -0400

    quick comment

--- test_slam.sh
+++ test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-xterm -e " source /opt/ros/noetic/setup.bash; roscore"
-sleep 2
+# xterm -e " source /opt/ros/noetic/setup.bash; roscore" &
+# sleep 2
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 3
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch" &
