855c7493d36070957b1ffb53ac6c68ccff09c629
Onur Bagoren <onurbagoren3@gmail.com>
2020-09-10 17:00:14 -0700

    Visualizing markers

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -21,23 +21,19 @@ class AddMarkers
         ros::Publisher marker_pub;;
         ros::Subscriber odom_sub;
         ros::Subscriber goal_sub;
+
+        vector< vector<float> > goals{ { 1.0, 3.0 }, { 4.0, 0.0 } };
+
         void odomCallBack( const nav_msgs::Odometry::ConstPtr &msg );
         void goalCallBack( const geometry_msgs::Pose &msg );
         void publishMarker( void );
         bool robotAtMarker( geometry_msgs::Pose &robot_position, geometry_msgs::Pose &goal_position );
-
-        float position_threshold = sqrt(2);
-        float orientation_threshold_w = 0.1;
-
-        bool searching = false;
 };
 
 AddMarkers::AddMarkers()
 {
     marker_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 10 );
-    goal_sub = n.subscribe("/goal", 1000, &AddMarkers::goalCallBack, this);
-    ROS_INFO( "Marker position: %f, %f", marker_position.position.x, marker_position.position.y );
-    odom_sub = n.subscribe("/odom", 1000, &AddMarkers::odomCallBack, this);
+
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time::now();
 
@@ -72,24 +68,6 @@ AddMarkers::~AddMarkers()
 
 }
 
-void AddMarkers::odomCallBack( const nav_msgs::Odometry::ConstPtr &msg )
-{
-    robot_pose.position.x = msg->pose.pose.position.x;
-    robot_pose.position.y = msg->pose.pose.position.y;
-    robot_pose.position.y = msg->pose.pose.position.y;
-    robot_pose.orientation.x = msg->pose.pose.orientation.x;
-    robot_pose.orientation.y = msg->pose.pose.orientation.y;
-    robot_pose.orientation.z = msg->pose.pose.orientation.z;
-    robot_pose.orientation.w = msg->pose.pose.orientation.w;
-}
-
-void AddMarkers::goalCallBack( const geometry_msgs::Pose &msg )
-{
-    ROS_INFO( "In goal call back" );
-    marker_position.position.x = msg.position.x;
-    marker_position.position.y = msg.position.y;
-}
-
 void AddMarkers::publishMarker( void )
 {
     while( marker_pub.getNumSubscribers() < 1 )
@@ -97,49 +75,24 @@ void AddMarkers::publishMarker( void )
         ROS_WARN_ONCE("Create a subscriber to the marker");
         sleep(1);
     }
-
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position = marker_position.position;
-    marker_pub.publish(marker);
-    sleep(5);
-
-    // if(!searching)
-    // {
-    //     ROS_INFO("Searching");
-    //     marker.action = visualization_msgs::Marker::ADD;
-    //     marker.pose.position.x = marker_position.position.x;
-    //     marker.pose.position.y = marker_position.position.y;
-    //     marker_pub.publish(marker);
-    //     searching = true;
-    // }
-    // cout << "Marker_pose: " << marker.pose << endl;
-    // if( !robotAtMarker( robot_pose, marker.pose ) )
-    // {
-    //     ROS_INFO("Not at marker");
-    // }
-    // else
-    // {  
-    //     ROS_INFO("At marker");
-    //     searching = false;
-    //     marker.action = visualization_msgs::Marker::DELETE;
-    //     marker_pub.publish(marker);
-    //     sleep(5);
-    // }
-
-}
-
-bool AddMarkers::robotAtMarker( geometry_msgs::Pose &robot_position, geometry_msgs::Pose &goal_position )
-{
-    cout << robot_position.position.x << goal_position.position.x << endl;
-    float x_diff = robot_position.position.x - goal_position.position.x;
-    float y_diff = robot_position.position.y - goal_position.position.y;
-    ROS_INFO( "%f, %f", x_diff, y_diff );
-    float position_diff = sqrt( pow( x_diff, 2 ) + pow( y_diff, 2 ) );
-
-    if( position_diff < position_threshold )
-        return true;
     
-    return false;
+    for( int i = 0; i < goals.size(); i++ )
+    {
+        marker.pose.position.x = goals[i][0];
+        marker.pose.position.y = goals[i][1];
+
+        marker.action = visualization_msgs::Marker::ADD;
+
+        marker_pub.publish(marker);
+        sleep(5);
+        if( i != goals.size() - 1 )
+        {
+            marker.action = visualization_msgs::Marker::DELETE;
+            marker_pub.publish( marker );
+            sleep(5);
+        }
+    }
+
 }
 
 int main( int argc, char** argv )
